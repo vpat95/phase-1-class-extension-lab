bf9c925e509499b56e64a2bd62070674dc37a740
Vivek Patel <vpatel360@gmail.com>
2023-07-22 22:40:26 -0400

    commit tree df4b89b8f887b8a2d4f295722af31a0982636a96

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Your code here
+
+class Polygon{
+    constructor(arr){
+        this.sides = arr
+    }
+
+    get countSides(){
+        return this.sides.length
+    }
+
+    get perimeter(){
+        return this.sides.reduce((element, acc) => acc += element)
+    }   
+}
+
+class Triangle extends Polygon{
+    get isValid(){
+        if(this.sides[0]+this.sides[1] > this.sides[2] && this.sides[0]+this.sides[2] > this.sides[1] && this.sides[1]+this.sides[2] > this.sides[0]){
+            return true
+        }
+        else{
+            return false
+        }
+    }
+}
+
+class Square extends Polygon{
+    get isValid(){
+        if((this.perimeter)/4 === this.sides[0]){
+            return true
+        }
+        else {
+            return false
+        }
+    }
+
+    get area(){
+        return this.sides[0]*this.sides[1]
+    }
+}
